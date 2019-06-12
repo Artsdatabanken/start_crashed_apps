f602f4475aed0e3bcc07055e6c2608989739b1e2
Even <even.juberg@ntnu.no>
2019-06-12 16:17:56 +0200

    Added file.

---
+++ start_crashed_app.ps1
@@ -0,0 +1,10 @@
+﻿$apppools = @(c:\windows\system32\inetsrv\appcmd list apppool /state:Stopped)
+
+forEach ($apppool in $apppools) {
+
+#$name = Select-String -InputObject $apppool -Pattern '"([^"]+)"'
+
+$test = $apppool -Match '"([^"]+)"'
+$app = $Matches[1]
+Start-WebAppPool -Name $app
+}
