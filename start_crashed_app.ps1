68449e6b2611efc2d6078116ac13a149eeefcb54
Even <even.juberg@ntnu.no>
2019-06-13 10:07:32 +0200

    Added functionality to make exceptions by listing pools that should not be started.

--- start_crashed_app.ps1
+++ start_crashed_app.ps1
@@ -2,12 +2,35 @@
 $apppools = @(c:\windows\system32\inetsrv\appcmd list apppool /state:Stopped)
 #Path of loggfile
 $logging = 'D:\logs\pools\pools.txt'
+#Exceptions: list of exceptions
+$exceptions = @()
+$tobeexcempt = 0
+
 
 forEach ($apppool in $apppools) {
     
     $test = $apppool -Match '"([^"]+)"'
-    $app = $Matches[1]
-    echo $app
-    Start-WebAppPool -Name $app
-	(Get-date).ToString() + " , " + $app + " was started because it had stopped!" | Out-File -Append $logging
+    $apppool = $Matches[1]
+    forEach ($exception in $exceptions) {
+        $dump = $exception -Match '"([^"]+)"'
+        $exception = $Matches[1]
+        
+        if ($exception -eq $apppool) {
+            #Do nothing
+            $tobeexcempt = 1
+        } else {
+            #Do everything
+            $tobeexcempt = 0
+
+        }
+    }
+
+    if ($tobeexcempt -eq 0) {
+        Start-WebAppPool -Name $apppool
+	    (Get-date).ToString() + " , " + $apppool + " was started because it had stopped!" | Out-File -Append $logging
+    } else {
+        #Do nothing
+        $tobeexcempt = 0
+    }
+    
 }
