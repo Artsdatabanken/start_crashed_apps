cdef42ec08c215c3fc5c9898be73b2795df9b91f
Even <even.juberg@ntnu.no>
2019-06-13 09:31:20 +0200

    Added some comments and logging.

--- start_crashed_app.ps1
+++ start_crashed_app.ps1
@@ -1,8 +1,13 @@
-﻿$apppools = @(c:\windows\system32\inetsrv\appcmd list apppool /state:Stopped)
+﻿#Defining an array of stopped application pools
+$apppools = @(c:\windows\system32\inetsrv\appcmd list apppool /state:Stopped)
+#Path of loggfile
+$logging = 'D:\logs\pools\pools.txt'
 
 forEach ($apppool in $apppools) {
-
+    
     $test = $apppool -Match '"([^"]+)"'
     $app = $Matches[1]
+    echo $app
     Start-WebAppPool -Name $app
+	(Get-date).ToString() + " , " + $app + " was started because it had stopped!" | Out-File -Append $logging
 }
